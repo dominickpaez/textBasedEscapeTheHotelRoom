556274fee01c02d7275d8c8d868151bf5c4ccbfd
dominick.paez <superrrstyxx@gmail.com>
2018-11-28 08:54:43 -0800

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
-    //ha
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Commands:\nInteract (noun):Will interact with the given noun\nPick up (item): Will pick up given item and place in inventory\nUse (item) (noun): Will use given item on the noun\nCraft (item) (item): Will craft an item from the two items given\n");
+        System.out.println("You awaken in a barren room. You look out the window and you are in a highriser hotel.\nThe bedroom contains a; bed, nightstand, tv stand, the front door, and the bathroom door.");
+    }
 }
