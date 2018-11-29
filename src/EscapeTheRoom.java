71312a53b66ba2dfecfc0ef6b93e75fc76dbe9c6
dominick.paez <superrrstyxx@gmail.com>
2018-11-29 09:00:52 -0800

    who

--- src/EscapeTheRoom.java
+++ src/EscapeTheRoom.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Main {
+public class EscapeTheRoom {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Commands:\nInteract (noun):Will interact with the given noun\nPick up (item): Will pick up given item and place in inventory\nUse (item) (noun): Will use given item on the noun\nCraft (item) (item): Will craft an item from the two items given\n");
         System.out.println("You awaken in a barren room. You look out the window and you are in a highriser hotel.\nThe bedroom contains a; bed, nightstand, tv stand, the front door, and the bathroom door.");
+        //Initialize objects
+
+
+
+        
         boolean x = false;
         while (!x){
             x = promptUser(scan);
         }
-        Inventory bedInventory = new Inventory();
-        Entity bed = new Entity("bed",bedInventory);
+
     }
     public static boolean promptUser(Scanner scan) {
         System.out.print ("Enter command: ");
@@ -24,9 +28,12 @@ public class Main {
 
         }else if(splitted[0].equalsIgnoreCase("Craft")) {
 
+        }else if(splitted[0].equalsIgnoreCase("Quit")) {
+            return true;
         }else{
             System.out.println("Invalid input try again");
         }
         return false;
     }
+
 }
